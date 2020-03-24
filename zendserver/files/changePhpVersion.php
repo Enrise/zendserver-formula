eb4c0f66679d8587c1dff1428d6b34df22e5c23f
pc-ocapl <olegs.capligins@presentconnection.eu>
2020-03-24 12:04:12 +0200

    SIM-7778: change php version updated to conform the new ZendServerWebApi class, http retries count moved to pillar configuration

--- zendserver/files/changePhpVersion.php
+++ zendserver/files/changePhpVersion.php
@@ -1,41 +1,5 @@
 #!/usr/bin/env php
 <?php
 
-if ($argc < 4)
-{
-    echo "use like: php changePhpVersion.php user api_key 7.3\n";
-    exit();
-}
-
-$user = $argv[1];
-$apiKey = $argv[2];
-$phpVersion = $argv[3];
-
-function generateRequestSignature($host, $path, $timestamp, $userAgent, $apiKey)
-{
-    $data = $host . ":" .$path. ":" .$userAgent. ":" .gmdate('D, d M Y H:i:s ', $timestamp) . 'GMT';
-    return hash_hmac('sha256', $data, $apiKey);
-}
-
-$host = "localhost:10081";
-$path = "/ZendServer/Api/changePhpVersion";
-$userAgent = "PhpVersionChanger";
-$signature = generateRequestSignature($host, $path, time(), $userAgent, $apiKey);
-
-$ch = curl_init();
-
-curl_setopt($ch, CURLOPT_URL, "http://".$host.$path);
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, "phpVersion=".$phpVersion);
-
-$headers = [
-    'Accept: application/vnd.zend.serverapi+json;version=1.16',
-    'User-Agent: '. $userAgent,
-    'Content-Type: application/x-www-form-urlencoded; charset=utf-8',
-    'Date: '.gmdate('D, d M Y H:i:s ').'GMT',
-    'X-Zend-Signature: '. $user.";".$signature,
-];
-
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-$server_output = curl_exec($ch);
-curl_close($ch);
+require_once "ZendServerWebApi.php";
+(new ZendServerWebApi($argv))->setPhpVersion();
